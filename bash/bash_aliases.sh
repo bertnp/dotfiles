2e52100bfdfa0e183b1a06e206111fda72ee8d41
bertnp <bertnp@users.noreply.github.com>
2018-05-30 08:45:40 -0400

    Modify githash alias

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -7,7 +7,7 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias dic="sdcv"
-alias gh="git rev-parse HEAD > .githash"
+alias githash="git rev-parse --short HEAD"
 alias sshag='export SSH_AUTH_SOCK=`$repo_dir/sshag/sshag.sh`'
 # the extra characters confuse scripts that don't expect it
 # alias grep="grep --color=auto -n" # output line numbers
